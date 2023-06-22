8523b68ef89a8c25488bf36010c694af0545118c
Van Huynh <27980695+vdhuynh@users.noreply.github.com>
2023-06-22 13:45:55 -0500

    Create numbers.sql

---
+++ numbers.sql
@@ -0,0 +1,13 @@
+CREATE TABLE devices (
+    DeviceID INT,
+    DeviceName VARCHAR(255),
+    Price DECIMAL(10, 2)
+);
+
+SHOW TABLES;
+
+CREATE TABLE stock (
+  deviceID INT, 
+  quantity INT, 
+  totalPrice DECIMAL);
+);
